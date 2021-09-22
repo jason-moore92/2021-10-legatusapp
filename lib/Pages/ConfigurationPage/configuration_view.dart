0fc389d1d260f2dcb2d28b3e80b796cf024a8452
iscojin <flutterdev9715@gmail.com>
2021-09-22 09:12:05 +0100

    2021-09-22 02

--- lib/Pages/ConfigurationPage/configuration_view.dart
+++ lib/Pages/ConfigurationPage/configuration_view.dart
@@ -146,10 +146,10 @@ class _ConfigurationViewState extends State<ConfigurationView> with SingleTicker
     await _keicyProgressDialog!.show();
 
     _authProvider!.login(
-      // email: _email,
-      // password: _password,
-      email: "mobile@legatus.fr",
-      password: "BiksbcNgALR8",
+      email: _email,
+      password: _password,
+      // email: "mobile@legatus.fr",
+      // password: "BiksbcNgALR8",
     );
   }
 
